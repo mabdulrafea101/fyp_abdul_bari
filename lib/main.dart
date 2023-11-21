20991ddac942e539de3dd0b811a5b7c5988e3e57
mabdulrafea101 <mabdulrafea@gmail.com>
2023-11-21 17:05:40 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:office_furniture_store/src/view/screen/intro_screen.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
